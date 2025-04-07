fa98ce66b605f46cf223dab4c8dc3f2e4a167e14
Meryem Uludağlı <meryemuslu2035@gmail.com>
2025-04-07 21:09:41 +0300

    add

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,4 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import api from "../api";
+import { toast } from "react-toastify";
+import Form from "../components/Form";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
-  return <div>Create</div>;
+  const navigate = useNavigate();
+
+  // api isteği
+  const { isLoading, mutate } = useMutation({
+    mutationFn: (newRecipe) => api.post("/api/v1/recipes", newRecipe),
+
+    onSuccess: () => {
+      toast.success("Yeni Tarif Oluşturuldu");
+      navigate("/");
+    },
+
+    onError: () => {
+      toast.error("Bir sorun oluştu");
+    },
+  });
+
+  return (
+    <div>
+      <h1 className="text-red-400 text-3xl font-bold">Yeni Tarif Oluştur</h1>
+
+      <Form isLoading={isLoading} mutate={mutate} />
+    </div>
+  );
 };
+
 export default Create;
